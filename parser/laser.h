03660856b7250d5732105acc9d15b0363b97f726
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-01-24 16:55:43 +0100

    Change waist to w0 and sigma0 that can optionally be used in laser definition.  Consistency in wavelength naming rather than waveLength as was old variable which is wrong.
    Former-commit-id: be8c5fd94f0489f920bbedd214c99b7425a2ade8

--- parser/laser.h
+++ parser/laser.h
@@ -36,44 +36,42 @@ namespace GMAD {
   {
   public:
     std::string name;         ///< Name of this laser.
-        double waveLength;
-        double m2;
-        double pulseEnergy;
-        double pulseDuration;
-        double waist;
-
-
-        /// constructor
-        Laser();
-        /// reset
-        void clear();
-        /// print some properties
-        void print() const;
-        /// set methods by property name and value
-        template<typename T>
-        void set_value(std::string property, T value);
-
-    private:
-        /// publish members
-        void PublishMembers();
-    };
-
+    double wavelength;
+    double m2;
+    double pulseEnergy;
+    double pulseDuration;
+    double w0;
+    double sigma0;
+    
+    /// constructor
+    Laser();
+    /// reset
+    void clear();
+    /// print some properties
+    void print() const;
+    /// set methods by property name and value
     template<typename T>
-    void Laser::set_value(std::string property, T value) {
+    void set_value(std::string property, T value);
+    
+  private:
+    /// publish members
+    void PublishMembers();
+  };
+  
+  template<typename T>
+  void Laser::set_value(std::string property, T value) {
 #ifdef BDSDEBUG
-        std::cout << "Laser> Setting value " << std::setw(25) << std::left << property << value << std::endl;
+    std::cout << "Laser> Setting value " << std::setw(25) << std::left << property << value << std::endl;
 #endif
-        // member method can throw runtime_error, catch and exit gracefully
-        try {
-            set(this, property, value);
-        }
-        catch (std::runtime_error) {
-            std::cerr << "Error: placement> unknown option \"" << property << "\" with value " << value << std::endl;
-            exit(1);
-        }
+    // member method can throw runtime_error, catch and exit gracefully
+    try {
+      set(this, property, value);
     }
+    catch (std::runtime_error) {
+      std::cerr << "Error: placement> unknown option \"" << property << "\" with value " << value << std::endl;
+      exit(1);
+    }
+  }
 }
 
 #endif
-
-
