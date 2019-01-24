fe1d866cc22cf03a60c6fae134567b1962d4f438
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-01-24 16:02:32 +0100

    Start of extended logical volume.
    Former-commit-id: fa6e8191a68705211fdd1b71d6ac5526e202eb15

--- include/BDSLogicalVolumeLaser.hh
+++ include/BDSLogicalVolumeLaser.hh
@@ -16,4 +16,54 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 */
+#ifndef BDSLOGICALVOLUMELASER_H
+#define BDSLOGICALVOLUMELASER_H
+
+#include "G4LogicalVolume.hh"
+#include "G4Types.hh"
+
+class BDSLaser;
+class G4FieldManager;
+class G4Material;
+class G4UserLimits;
+class G4VSensitiveDetector;
+class G4VSolid;
+
+/** 
+ * @brief Extended logical volume with laser definition.
+ *
+ * @author Laurie Nevay
+ */
+
+class BDSLogicalVolumeLaser: public G4LogicalVolume
+{
+public:
+  BDSLogicalVolumeLaser(G4VSolid*             solid,
+			G4Material*           material,
+			const G4String&       name,
+			const BDSLaser*       laserIn,
+			G4FieldManager*       fieldManager      = nullptr,
+			G4VSensitiveDetector* sensitiveDetector = nullptr,
+			G4UserLimits*         userLimits        = nullptr,
+			G4bool                optimise          = true);
+  
+  virtual ~BDSLogicalVolumeLaser();
+
+  /// Access the laser.
+  const BDSLaser* Laser() const {return laser;}
+
+  /// Override flag function as to whether G4LogicalVolume is extended.
+  virtual G4bool IsExtended() const override {return true;}
+  
+private:
+  /// @{ Private copy and assignment constructors.
+  BDSLogicalVolumeLaser(const BDSLogicalVolumeLaser& other) = delete;
+  BDSLogicalVolumeLaser& operator=(const BDSLogicalVolumeLaser& other) = delete;
+  /// @}
+  
+  /// Laser instance - owned by this class.
+  BDSLaser* laser;
+};
+
+#endif
 
