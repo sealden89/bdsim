a044e174555cb9532689d702755a84c81b2f05eb
siobhanalden <siobhanalden@gmail.com>
2019-01-24 20:07:34 +0000

    updates to laserwire development discrete process
    Former-commit-id: c1d7339f0490913f368817bd4df1fada47bdc9f3

--- src/BDSLaserPhotoDetachment.cc
+++ src/BDSLaserPhotoDetachment.cc
@@ -23,6 +23,7 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #include "G4LogicalVolume.hh"
 #include "G4ProcessType.hh"
 #include "G4Track.hh"
+#include "Randomize.hh"
 
 #include "CLHEP/Units/PhysicalConstants.h"
 #include "CLHEP/Units/SystemOfUnits.h"
@@ -56,16 +57,48 @@ G4double BDSLaserPhotoDetachment::GetMeanFreePath(G4Track& track,
   return mfp;
 }
 
-G4VParticleChange BDSLaserPhotoDetachment::PostStepDoIt()
+G4VParticleChange BDSLaserPhotoDetachment::PostStepDoIt(G4Track& aTrack)
 {
-
-    //G4Material* aMaterial = trackData.GetMaterial();
-
- //   if(aMaterial == BDSMaterials::Instance()->GetMaterial("laserVac"))
-  //  {
+  /*
+  aParticleChange.Initialize(aTrack);
 
 
+   ////////////////////////////////////////////////////////
+//copied from mfp to access laser instance is clearly incorrect!
 
-    //}
+  G4ThreeVector particlePosition = aTrack.GetPolarization();
+  G4double radius = std::sqrt(particlePosition[0]*particlePosition[0]+particlePosition[1]*particlePosition[1])
+
+  G4LogicalVolume* lv = aTrack.GetVolume()->GetLogicalVolume();
+  if (!lv->IsExtended())
+  {// not extended so can't be a laser logical volume
+    return DBL_MAX;
+  }
+  BDSLogicalVolumeLaser* lvv = dynamic_cast<BDSLogicalVolumeLaser*>(lv);
+  if (!lvv)
+  {// it's an extended volume but not ours (could be a crystal)
+    return DBL_MAX;
+  }
+
+  // else proceed
+  const BDSLaser* laser = lvv->Laser();
+
+   /////////////////////////////////////////////////////////////
+
+  G4double photonDensity = laser->Intensity(radius, particlePosition[2])/laser->PhotonEnergy();
+  G4double crossSection = 3.2e-21*CLHEP::m2; // m^2 hard coded for now
+  G4double stepTime = aTrack.GetStepLength()/aTrack.GetVelocity();//this seems bad as its not directional
+//also why is G4Track.GetVelocity a G4double not vector
+  G4double photoDetachmentProbability = 1.0-std::exp(-1.0*crossSection*photonDensity*stepTime);
+
+  G4double randomNumber = G4UniformRand();
+
+  if(photoDetachmentProbability>=randomNumber)
+  {
+    //create secondary electron and neutralise the other plus step out of volume with large mfp
+
+  }
+
+*/
 
 }
