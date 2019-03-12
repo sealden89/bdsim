afae5fa72d83730eb717ddcd7734d281005d6f5f
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2019-03-12 22:14:31 +0100

    Shrink a beam pipe aperture definition.
    Former-commit-id: bd7625a48e8539978f70da628c551e8538212b32

--- src/BDSBeamPipeInfo.cc
+++ src/BDSBeamPipeInfo.cc
@@ -27,6 +27,7 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #include "globals.hh" // geant4 types / globals
 #include "G4Material.hh"
 
+#include <algorithm>
 
 BDSBeamPipeInfo::BDSBeamPipeInfo(BDSBeamPipeType beamPipeTypeIn,
 				 G4double        aper1In,
@@ -126,6 +127,25 @@ BDSBeamPipeInfo::BDSBeamPipeInfo(BDSBeamPipeInfo* defaultInfo,
     {beamPipeMaterial = BDSMaterials::Instance()->GetMaterial(beamPipeMaterialIn);}
   
   CheckApertureInfo();
+}
+
+BDSBeamPipeInfo BDSBeamPipeInfo::ShrinkBy(G4double margin) const
+{
+  BDSBeamPipeInfo result(*this); // make a copy
+
+  // in all cases reducing all the individual parameters by a margin works
+  // none should go below zero though
+  G4double smallest = std::min(std::min(result.aper1, result.aper2), std::min(result.aper3, result.aper4));
+  if (margin > smallest)
+    {
+      G4String message = "Shrinking by margin " + std::to_string(margin) + " for beam pipe would result in sub zero aperture.";
+      throw BDSException(message);
+    }
+  result.aper1 -= margin;
+  result.aper2 -= margin;
+  result.aper3 -= margin;
+  result.aper4 -= margin;
+  return result;
 }
   
 void BDSBeamPipeInfo::CheckApertureInfo()
