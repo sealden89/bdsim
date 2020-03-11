1f776d065afe5164b6238805caa8057df20bd815
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2020-03-11 17:26:17 +0100

    Example and tidying.
    Former-commit-id: 650f54286ea85e9aadec31e829115de7ba796112

--- include/BDSLaser.hh
+++ include/BDSLaser.hh
@@ -18,7 +18,8 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef BDSLASER_H
 #define BDSLASER_H
-#include "globals.hh" // geant4 types / globals
+#include "G4String.hh"
+#include "G4Types.hh"
 #include "G4ThreeVector.hh"
 
 #include "CLHEP/Units/SystemOfUnits.h"
@@ -26,7 +27,7 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #include <vector>
 
 /**
- * @brief Describe the function here
+ * @brief Class to provide laser intensity at any point.
  *
  * @author Siobhan Alden
  */
