cf4cefdfca6204bb7794defca7e6512dc4d42e9a
siobhanalden <siobhanalden@gmail.com>
2020-09-04 15:29:45 +0100

    Changing peak power to be calculated with correct laser pulse duration.
    Former-commit-id: 82b5e5c1226b5de108a2b75ae457a8a220368a3d

--- src/BDSLaser.cc
+++ src/BDSLaser.cc
@@ -69,8 +69,7 @@ BDSLaser::BDSLaser(G4double wavelengthIn,
 {
   if(!BDS::IsFinite(sigma0In))
     {throw BDSException(__METHOD_NAME__, "Laser waist sigma0 is zero.");}
-  
-  peakPower = pulseEnergy / pulseDuration;
+  peakPower = pulseEnergy / ((2.0*std::sqrt(2.0*std::log(2.0)))*pulseDuration);
   rayleighRange = (CLHEP::pi * (2.0*sigma0)*(2.0*sigma0)) / (wavelength * m2);
 }
 
