97f5cc308040f7a56b9cefaf7098b3aa33fc8c88
Laurie Nevay <laurie.nevay@cern.ch>
2024-10-13 21:49:07 +0200

    Write out vis attributes of logical volumes as aux tags in gdml.
    Former-commit-id: f2d7bd16164ae5909a2bea9f07c851f584496f2e

--- include/BDSGeometryWriter.hh
+++ include/BDSGeometryWriter.hh
@@ -20,8 +20,16 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #define BDSGEOMETRYWRITER_H
 
 #include "globals.hh" // geant4 types / globals
+#include "G4VisAttributes.hh"
+#include "G4String.hh"
 
+#include <map>
+
+class G4Colour;
+class G4GDMLParser;
+class G4LogicalVolume;
 class G4VPhysicalVolume;
+struct G4GDMLAuxStructType;
 
 /**
  * @brief A class for writing fully constructed geometry from BDSIM
@@ -36,7 +44,10 @@ public:
   BDSGeometryWriter();
   ~BDSGeometryWriter();
 
-  /// Dispatch function that matchest the geometry type and uses the appropraite
+  /// Static map of strings of drawing styles.
+  static const std::map<G4VisAttributes::ForcedDrawingStyle, G4String> drawStyle;
+
+  /// Dispatch function that matches the geometry type and uses the appropriate
   /// member function to write out the geometry
   void ExportGeometry(const G4String& geometryType,
                       const G4String& geometryFileName);
@@ -49,6 +60,22 @@ private:
   /// the world volume will be written out.
   void WriteToGDML(const G4String& outputFileName, G4VPhysicalVolume* volume = nullptr);
 
+  /// Generate a map of logical volume pointers to output auxiliary structure
+  /// information for writing out to GDML.
+  static std::map<G4LogicalVolume*, G4GDMLAuxStructType> PrepareColourInformation(G4VPhysicalVolume* volume);
+
+  /// Register the auxiliary information with the GDML parser (writer).
+  static void RegisterVolumeAuxiliaryInformation(G4GDMLParser& parser,
+                                                 const std::map<G4LogicalVolume*, G4GDMLAuxStructType>& volToAuxMap);
+
+  /// Return a string with the format "int float float float float" for bool
+  /// visible, rgb, and alpha values separated by a space.
+  static G4String ColourToRGBAString(G4bool visible,
+                                     const G4Colour& colour);
+
+  /// Recursively add information the map by reference for each logical
+  /// volume vis attributes if they exist. Default is surface style.
+  static void AddLVColour(std::map<G4LogicalVolume*, G4GDMLAuxStructType>&, G4LogicalVolume* lv);
 #endif
 };
 
--- src/BDSGeometryWriter.cc
+++ src/BDSGeometryWriter.cc
@@ -22,14 +22,29 @@ along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
 #include "BDSUtilities.hh"
 
 #include "globals.hh" // geant4 types / globals
+
 #ifdef USE_GDML
+#include "G4Colour.hh"
+#include "G4GDMLAuxStructType.hh"
 #include "G4GDMLParser.hh"
+#include "G4VisAttributes.hh"
 #endif
+
 #include "G4FileUtilities.hh"
 #include "G4String.hh"
 #include "G4TransportationManager.hh"
 
+#include <array>
+#include <map>
+#include <sstream>
+
 class G4VPhysicalVolume;
+
+const std::map<G4VisAttributes::ForcedDrawingStyle, G4String> BDSGeometryWriter::drawStyle = {
+  {G4VisAttributes::ForcedDrawingStyle::solid,     "solid"},
+  {G4VisAttributes::ForcedDrawingStyle::wireframe, "wireframe"},
+  {G4VisAttributes::ForcedDrawingStyle::cloud,     "cloud"},
+};
 
 BDSGeometryWriter::~BDSGeometryWriter()
 {;}
@@ -48,6 +63,8 @@ void BDSGeometryWriter::ExportGeometry(const G4String& geometryType,
       throw BDSException(__METHOD_NAME__, "Unable to write out " + geometryFileName + ", as compiled without GDML support");
 #endif
     }
+  else
+    {throw BDSException(__METHOD_NAME__, "unknown geometry export type \"" + geometryType + "\".");}
 }
 
 #ifdef USE_GDML
@@ -73,7 +90,66 @@ void BDSGeometryWriter::WriteToGDML(const G4String&    outputFileName,
         {throw BDSException(__METHOD_NAME__, "directory \"" + dirName + "\" does not exist - please create it first.");}
     }
 
+  auto volToAuxMap = PrepareColourInformation(volume);
+
   G4GDMLParser parser;
+  RegisterVolumeAuxiliaryInformation(parser, volToAuxMap);
   parser.Write(outputFileName, volume, true);
 }
+
+std::map<G4LogicalVolume*, G4GDMLAuxStructType> BDSGeometryWriter::PrepareColourInformation(G4VPhysicalVolume* volume)
+{
+  std::map<G4LogicalVolume*, G4GDMLAuxStructType> result;
+  auto lv = volume->GetLogicalVolume();
+  AddLVColour(result, lv);
+  return result;
+}
+
+void BDSGeometryWriter::RegisterVolumeAuxiliaryInformation(G4GDMLParser& parser,
+                                                           const std::map<G4LogicalVolume*, G4GDMLAuxStructType>& volToAuxMap)
+{
+  for (const auto& lvAux : volToAuxMap)
+    {parser.AddVolumeAuxiliary(lvAux.second, lvAux.first);}
+}
+
+G4String BDSGeometryWriter::ColourToRGBAString(G4bool visible,
+                                               const G4Colour& colour)
+{
+  std::array<G4double, 4> numbers = {colour.GetRed(),
+                                     colour.GetGreen(),
+                                     colour.GetBlue(),
+                                     colour.GetAlpha()};
+  if (!visible)
+    {numbers[3] = 0;}
+  std::stringstream ss;
+  ss << visible << " ";
+  for (auto cv : numbers)
+    {ss << std::setprecision(4) << cv << " ";} // colours are normalised 0 to 1.
+  G4String result = ss.str();
+  result.pop_back(); // remove last space
+  return result;
+}
+
+void BDSGeometryWriter::AddLVColour(std::map<G4LogicalVolume*, G4GDMLAuxStructType>& map, G4LogicalVolume* lv)
+{
+  const G4VisAttributes* visAttr = lv->GetVisAttributes();
+  if (visAttr)
+    {
+      G4GDMLAuxStructType c;
+      c.type = "bdsim_colour_and_style";
+      c.value = ColourToRGBAString(visAttr->IsVisible(), visAttr->GetColour());
+      if (visAttr->IsForceDrawingStyle())
+        {c.unit = drawStyle.at(visAttr->GetForcedDrawingStyle());}
+      else
+        {c.unit = drawStyle.at(G4VisAttributes::ForcedDrawingStyle::solid);} // default
+      c.auxList = nullptr;
+      map[lv] = c;
+    }
+  for (std::size_t i = 0; i < lv->GetNoDaughters(); i++)
+    {
+      auto dlv = lv->GetDaughter(i)->GetLogicalVolume();
+      AddLVColour(map, dlv);
+    }
+}
+
 #endif
