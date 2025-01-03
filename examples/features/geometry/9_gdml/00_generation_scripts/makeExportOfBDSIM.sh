7ad415dcd0b796da259bb78a00032d0f08eea0b4
Laurie Nevay <laurie.nevay@cern.ch>
2025-01-03 14:00:05 +0100

    Reduce duration of test from preprocessing. Preprocessing must be on as it's a bdsim export and the material names would clash.

--- examples/features/geometry/9_gdml/00_generation_scripts/makeExportOfBDSIM.sh
+++ examples/features/geometry/9_gdml/00_generation_scripts/makeExportOfBDSIM.sh
@@ -1,1 +1,5 @@
-bdsim --file=../../10_multiple_beamlines/multiplebeamlines.gmad --output=none --exportGeometryTo=../gdmls/multiple_beamlines_w_colour.gdml --batch
+# takes too long for preprocessing as too many volumes
+#bdsim --file=../../10_multiple_beamlines/multiplebeamlines.gmad --output=none --exportGeometryTo=../gdmls/multiple_beamlines_w_colour.gdml --batch
+
+
+bdsim --file=../../../../collimation/collimation.gmad --output=none --exportGeometryTo=../gdmls/collimation_export_w_colour.gdml --batch
