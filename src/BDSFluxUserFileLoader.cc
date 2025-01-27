e3a598bd29b3738bf25052e6367340bd3aa17c56
Siobhan Alden <siobhanalden@Siobhans-MacBook-Pro.local>
2025-01-27 09:21:42 +0000

    Octree updates for user defined laser flux

--- src/BDSFluxUserFileLoader.cc
+++ src/BDSFluxUserFileLoader.cc
@@ -1,3 +1,135 @@
-//
-// Created by Siobhan Alden on 21/01/2025.
-//
+/*
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway,
+University of London 2001 - 2024.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+
+author :: S Alden
+*/
+/*
+#include "BDSFluxUserFileLoader.hh"
+
+#include "BDSDebug.hh"
+#include "BDSException.hh"
+
+#include "globals.hh"
+#include "G4String.hh"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <exception>
+#include <map>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#ifdef USE_GZSTREAM
+#include "src-external/gzstream/gzstream.h"
+#endif
+
+template <class T>
+BDSFluxUserFileLoader<T>::BDSFluxUsderFileLoader()
+{;}
+
+template <class T>
+BDSFluxUserFileLoader<T>::~BDSFluxUserFileLoader()
+{;}
+
+template <class T>
+std::map<G4String, G4double> BDSFluxUserFileLoader<T>::Load(const G4String& fileName)
+{
+  T file;
+
+  file.open(fileName);
+
+  // test if file is valid
+#ifdef USE_GZSTREAM
+  bool validFile = file.rdbuf()->is_open();
+#else
+  bool validFile = file.is_open();
+#endif
+
+  if (!validFile)
+    {throw BDSException(__METHOD_NAME__, "Cannot open file \"" + fileName + "\"");}
+  else
+    {G4cout << "BDSFluxUserFileLoader::Load> loading \"" << fileName << "\"" << G4endl;}
+
+  std::string line;
+
+  std::map<G4String, G4double> flux;
+  G4int lineNum = 1;
+  while (std::getline(file, line))
+    { // read a line only if it's not a blank one
+      std::istringstream lines(line);
+      std::string xcoord;
+      std::string ycoord;
+      std::string zcoord;
+
+      std::string intensityValueString;
+
+      // skip a line if it's only whitespace
+      if (std::all_of(line.begin(), line.end(), isspace))
+        {continue;}
+
+      lines >> xcoord >> ycoord >> zcoord >> flux;
+
+      // exit if anything after the importance value
+      if (!lines.eof())
+	{
+	  std::string remainder;
+	  lines >> remainder;
+	  G4String message = "Error: Unknown value \"" + remainder + "\" in line " + std::to_string(lineNum) + " of the fluxUserFile";
+          throw BDSException(message);
+	}
+
+      // exit if no importance value is supplied
+      if (intensityValueString.empty())
+        {
+          G4String message = "No flux value was found for coordinate: x= \"" + xcoord + "\", y= \"" + ycoord + "\", z= \"" + zcoord + "\" in the importanceMapFile.";
+          throw BDSException(message);
+        }
+
+      G4double intensityValue = 0;
+      try
+	{intensityValue = std::stod(intensityValueString);}
+      catch (...)
+	{
+	  G4String message = "Error: Cell \"" + volume + "\" has importance value \"" + importanceValueString + "\",";
+          message += " importance value must be numeric.";
+          throw BDSException(message);
+	}
+
+      importance[volume] = importanceValue;
+
+      lineNum += 1;
+    }
+
+  file.close();
+
+  G4cout << "BDSImportanceFileLoader::Load> loaded " << importance.size() << " importance entries" << G4endl;
+
+  return importance;
+}
+
+template class BDSImportanceFileLoader<std::ifstream>;
+
+#ifdef USE_GZSTREAM
+template class BDSImportanceFileLoader<igzstream>;
+#endif
+
+*/
+
--- src/BDSOctree.cc
+++ src/BDSOctree.cc
@@ -1,3 +1,225 @@
-//
-// Created by Siobhan Alden on 24/01/2025.
-//
+
+
+/*
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway,
+University of London 2001 - 2024.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "BDSOctree.hh"
+
+
+Octree::Octree(const G4ThreeVector lowerBoundsIn,
+             const G4ThreeVector upperBoundsIn):
+             lowerBounds(lowerBoundsIn),
+             upperBounds(upperBoundsIn)
+{}
+
+Octree::~Octree(){
+  for (auto child : children) {
+    delete child; }
+}
+
+void Octree::insert(G4ThreeVector point, G4double data)
+{
+  if (isLeaf)
+  {
+    if (dataPoint.isSet)
+    {
+      // create children by default all 8 with half bounds of existing octree
+      createChildren();
+      // work out which of them the point belongs in
+      Octree* newOctant = childToSearch(point);
+      // add point with that .insert.
+      newOctant->insert(point,data);
+      // get old data point and find out which octant it belongs in
+      Octree* secondOctant = childToSearch({dataPoint.x,dataPoint.y,dataPoint.z});
+      secondOctant->insert({dataPoint.x,dataPoint.y,dataPoint.z},dataPoint.data);
+      setIsLeafFalse();
+    }
+    else
+    {
+      setIsLeafTrue();
+      dataPoint.x = point[0];
+      dataPoint.y = point[1];
+      dataPoint.z = point[2];
+      dataPoint.data = data;
+      dataPoint.isSet = true;
+    }
+  }
+
+  else
+  {
+    if (children[0]!= nullptr)
+      {
+        // binary search which octant this belongs in
+        // recurse this function on that octree
+      Octree* newOctant = childToSearch(point);
+      newOctant->insert(point,data);
+      }
+    else
+      {
+      // create children by default all 8 with half bounds of existing octree
+      createChildren();
+      // work out which of them the point belongs in
+      Octree* newOctant = childToSearch(point);
+      // add point with that .insert.
+      newOctant->insert(point,data);
+      }
+  }
+
+}
+
+void Octree::setParent(Octree* parentToAssign)
+{
+  this->parent = parentToAssign;
+}
+
+void Octree::createChildren()
+{
+  G4double midX = (upperBounds[0] - lowerBounds[0])/2.0;
+  G4double midY = (upperBounds[1] - lowerBounds[1])/2.0;
+  G4double midZ = (upperBounds[2] - lowerBounds[2])/2.0;
+  G4double upX = upperBounds[0];
+  G4double upY = upperBounds[1];
+  G4double upZ = upperBounds[2];
+  G4double lowX = lowerBounds[0];
+  G4double lowY = lowerBounds[1];
+  G4double lowZ = lowerBounds[2];
+  // filled in  backwards Z pattern top layer, top right first each layer
+  children[0] = new Octree({midX, midY, midZ}, upperBounds);
+  children[0]->setParent(this);
+  children[0]->setIsLeafTrue();
+  children[1] = new Octree({lowX, midY, midZ}, {midX, upY, upZ});
+  children[1]->setParent(this);
+  children[1]->setIsLeafTrue();
+  children[2] = new Octree({midX, lowY, midZ},{upX, midY, upZ});
+  children[2]->setParent(this);
+  children[2]->setIsLeafTrue();
+  children[3] = new Octree({lowX,lowY, midZ},{midX, midY, upZ});
+  children[3]->setParent(this);
+  children[3]->setIsLeafTrue();
+  children[4] = new Octree({midX, midY, lowZ},{upX, upY, midZ});
+  children[4]->setParent(this);
+  children[4]->setIsLeafTrue();
+  children[5] = new Octree({lowX, midY, lowZ},{midX, upY, midZ});
+  children[5]->setParent(this);
+  children[5]->setIsLeafTrue();
+  children[6] = new Octree({midX, lowY, lowZ}, {upX, midY, midZ});
+  children[6]->setParent(this);
+  children[6]->setIsLeafTrue();
+  children[7] = new Octree(lowerBounds, {midX, midY, midZ});
+  children[7]->setParent(this);
+  children[7]->setIsLeafTrue();
+
+
+}
+// consider binary search and then using bool logic to return the correct child instead of looping over all of them? Faster but more lines of code
+
+Octree* Octree::childToSearch(G4ThreeVector coords)
+{
+  G4double midX = (upperBounds[0] - lowerBounds[0])/2.0;
+  G4double midY = (upperBounds[1] - lowerBounds[1])/2.0;
+  G4double midZ = (upperBounds[2] - lowerBounds[2])/2.0;
+  G4int index = -1; // Initialize index with an invalid value
+  for (size_t i = 0; i < children.size(); i++) {
+    if (children[i] && children[i]->isPointInOctant(coords)) { // Check if children[i] is not null
+      index = static_cast<int>(i);
+      break;
+    }
+  }
+  if (index == -1) {
+    //need bds exception
+    throw std::runtime_error("No child contains the point in its octant.");
+  }
+  // Return the child at the valid index
+  return children[index];
+}
+
+G4bool Octree::isPointInOctant(G4ThreeVector point)
+{
+  if ((point[0] <= lowerBounds[0] || point[0] >= upperBounds[0]) &&
+      (point[1] <= lowerBounds[1] || point[1] >= upperBounds[1]) &&
+      (point[2] <= lowerBounds[2] || point[2] >= upperBounds[2]))
+  {return true;}
+  else {return false;}
+}
+
+double Octree::findNearestData(G4ThreeVector incomingCoordinates)
+{
+  if (isLeaf)
+  {return dataPoint.data*scaleFactor;}
+  else
+    {
+    Octree* newOctant = childToSearch(incomingCoordinates);
+    return newOctant->findNearestData(incomingCoordinates);
+    }
+}
+
+G4double Octree::distanceToData(Element data, G4ThreeVector incomingCoordinates)
+{
+  return std::sqrt((incomingCoordinates[0]-data.x)*(incomingCoordinates[0]-data.x)
+                   + (incomingCoordinates[1]-data.y)*(incomingCoordinates[1]-data.y)
+                   + (incomingCoordinates[2]-data.z)*(incomingCoordinates[2]-data.z));
+}
+void Octree::setCorners()
+{
+  corners[0] = upperBounds;
+  corners[1] = {lowerBounds[0], upperBounds[1], upperBounds[2]};
+  corners[2] = {upperBounds[0], lowerBounds[1], upperBounds[2]};
+  corners[3] = {lowerBounds[0], lowerBounds[1], upperBounds[2]};
+  corners[4] = {upperBounds[0], upperBounds[1], lowerBounds[2]};
+  corners[5] = {lowerBounds[0], upperBounds[1], lowerBounds[2]};
+  corners[6] = {upperBounds[0], lowerBounds[1], lowerBounds[2]};
+  corners[7] = lowerBounds;
+}
+
+/*double Octree::distanceToNode(Octree* node, std::array<double, 3> incomingCoordinates)
+{
+  setCorners();
+  std::array<double, 8> distances;
+  for (size_t i = 0; i<corners.size(); i++)
+    {
+    distances[i] = std::sqrt((incomingCoordinates[0]-corners[i][0])*(incomingCoordinates[0]-corners[i][0])
+                             + (incomingCoordinates[1] - corners[i][1])*(incomingCoordinates[1] - corners[i][1])
+                             + (incomingCoordinates[2] - corners[i][2])*(incomingCoordinates[2] - corners[i][2]));
+    }
+
+  auto minDistance = *std::min_element(distances.begin(), distances.end());
+  return minDistance;
+}
+*/
+/*
+std::vector<double> NearestKPoints(int k, std::array<double, 3> incomingCoordinates)
+{
+  std::vector<double><k,0> closestDataPoints;
+  Octree* containingOctantLeaf = childToSearch(incomingCoordinates);
+  closestDataPoints[0] = containingOctantLeaf->getDataPoint();
+
+
+
+
+
+// find the closest point.
+//populate the closest list from the siblings
+// go up in the octree and calculate the shortest distance to the nodes
+// if the nodes are closer than the values in the shortest list then search the octants of those nodes.
+// else return the nearest points.
+
+
+  return 0;
+
+}
+*/
