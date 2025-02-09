6ef813ea190492d595f4abd043ffdd2871e88f95
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-02-09 03:21:51 +0000

    Brew python mac os (#22)
    * CI/CD - Fix python3.13 overwrite problem for macOS-13 (intel)

--- .github/bin/install-deps-macos.sh
+++ .github/bin/install-deps-macos.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Update brew
 brew update
 
-brew install root wget clhep root bison xerces-c
+# --force-bottle python as other method fails with link error
+brew install python@3.13 --force-bottle
+
+# upgrade packages
+brew upgrade
+
+# install BDSIM deps
+brew install wget clhep bison xerces-c root
